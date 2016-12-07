21ed78d701a4bf253fa61c548cc1c43fac74b20c
prog <iks.gog@gmail.com>
2016-12-07 11:36:35 +0300

    Add Product class

---
+++ src/main/java/model/Catalog.java
@@ -0,0 +1,29 @@
+package model;
+
+public abstract class Catalog {
+	private int id;
+	private String descr;
+	
+	
+	public Catalog(int id, String descr) {
+		super();
+		this.id = id;
+		this.descr = descr;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	
+	
+}
---
+++ src/main/java/model/Product.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Product extends Catalog {
+
+	public Product(int id, String descr) {
+		super(id, descr);
+	}
+
+}
